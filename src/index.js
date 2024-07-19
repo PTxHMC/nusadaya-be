992d91fe48ec99919a3de2b39565031868e3450b
adzaky <muhadlim17@gmail.com>
2024-07-19 15:27:03 +0800

    feat: update cors origin url

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://nusadaya-fe.vercel.app/" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
